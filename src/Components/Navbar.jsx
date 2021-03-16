156b3a6838f925b7c0e020adfcbf85a1bdf137d2
Walter Guerrero <wsguerrero@gmail.com>
2021-03-15 22:38:10 -0600

    Navbar basic structure

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
 
 function Navbar() {
+    const [sidebar, setSidebar] = useState(false)
+
+    const showSidebar =() => setSidebar(!sidebar)
+
     return (
         <>
             <div className='navbar'>
                 <Link to='#' className='menu-bars'>
-                    <FaIcons.FaBars />
+                    <FaIcons.FaBars onClick={showSidebar}/>
                 </Link>
 
             </div>
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items'>
+                    <li className='navbar-toggle'>
+                        <Link to='#' className='menu-bars'>
+                            <AiIcons.AiOutlineCloseCircle />
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </>
     )
 }

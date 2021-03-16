4fe506bffd4e50d1afefb02fa7d38a4dd4bc82e3
Walter Guerrero <wsguerrero@gmail.com>
2021-03-15 23:37:22 -0600

    applied styling to Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Navbar.css';
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
